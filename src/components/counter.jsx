c530c2753a73f8f24bdb71507c9386f2f59befdf
Atharva <atharva7275@gmail.com>
2021-06-29 19:29:16 +0530

    React Native first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+// import React ,{Components} from 'React';
+import React, { Component } from 'react';
+
+// export default class Counter extends Component {
+//     render() { 
+//         return <h1>Hello World</h1>;
+//     }
+// }
+const Counter = () =>{
+    return ( 
+        <h1>Hello World</h1>
+    );
+}
+
+ 
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// const element = <h3>Hello World</h3>;//JSX:JSXML Element, Virtual react DOM Element
+// ReactDOM.render(element, document.getElementById('root'));
+// React.createElement(element);
+// console.log(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
