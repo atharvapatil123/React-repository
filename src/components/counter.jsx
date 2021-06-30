437173aeca1aa78c5ef902680448a53a5dbe77b8
Atharva <atharva7275@gmail.com>
2021-06-30 23:59:28 +0530

    done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,18 @@
 // import React ,{Components} from 'React';
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // export default class Counter extends Component {
-//     render() { 
+//     render() {
 //         return <h1>Hello World</h1>;
 //     }
 // }
-const Counter = () =>{
-    return ( 
-        <h1>Hello World</h1>
-    );
-}
+const Counter = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button>Increment</button>
+    </div>
+  );
+};
 
- 
 export default Counter;
